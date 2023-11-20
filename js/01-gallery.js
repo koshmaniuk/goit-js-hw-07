0b6f450212e73e5af16f746cd3fb8045d40420bb
koshmaniuk <trident93@gmail.com>
2023-11-20 01:17:54 -0500

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery")
+
+const markup = galleryItems.map(({preview, original, description}) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+    </a>
+    </li>`).join("")
+    
+list.insertAdjacentHTML('beforeend', markup)
+
+list.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    event.preventDefault()
+
+if (event.target === event.currentTarget) {
+    return;
+}
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600"/>`, {
+        onShow: (instance) => {document.addEventListener("keydown", escPress)},
+        onClose: (instance) => {document.removeEventListener("keydown", escPress)}
+    })
+    instance.show()
+
+    function escPress(event) {
+        if (event.code === "Escape") {
+            instance.close()
+        }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery")
+
+const markup = galleryItems.map(({preview, original, description}) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+    </li>`).join("")
+    
+list.insertAdjacentHTML('beforeend', markup)
+
+list.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    event.preventDefault()
+    
+    const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250});
+    lightbox.open(1)
+}
